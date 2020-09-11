89e9e89e259551be2c12d0b08f7483d1f5a64031
Keunung Park <pku928@naver.com>
2020-09-11 22:06:56 +0900

    dataframe column1

--- algorithms/programmers/sorting/가장 큰 수.py
+++ algorithms/programmers/sorting/가장 큰 수.py
@@ -1,7 +1,75 @@
 ans = ''
-numbers = [3, 30, 34, 5, 9, 35, 37, 33, 31,32]
-s_numbers = [str(n) for n in numbers]
+numbers = [3, 30, 34, 5, 9, 35, 37, 33, 31,32,0]
+n_l = list(map(str,numbers))
 
-sort_n  = sorted(s_numbers, key=lambda x : int(x[0]))[::-1]
-print(sort_n)
-for i in range(9,0,-1):
+my_list = []
+
+for k in range(9,-1,-1):
+    temp = [n for n in n_l if int(n[0])==k]
+    if temp :
+        my_list.append(temp)
+print(my_list)
+
+def rel_nums(a,b):
+    if int('a'+'b') > int('b'+'a'):
+        return ['a','b']
+    else :
+        return ['b','a']
+
+for lst in my_list:
+    for n in range(0,len(lst)):
+
+
+
+# def rel_nums(a,b):
+#     k = 0
+#     s_a = str(a); l_a = len(s_a)
+#     s_b = str(b); l_b = len(s_b)
+#     l = min(l_a, l_b)
+#     if l_a>l_b:
+#         flag = 0
+#     elif l_a<l_b:
+#         flag = 1
+#     else:
+#         flag = 2
+#
+#     if flag == 2 :
+#         while True:
+#             if s_a[k] > s_b[k]:
+#                 return [a,b]
+#             elif s_a[k] < s_b[k]:
+#                 return [b,a]
+#             else:
+#                 k += 1
+#             if k==l:
+#                 return [a, b]
+#
+#     elif flag == 1 :
+#         while True:
+#             if s_a[k] > s_b[k]:
+#                 return [a, b]
+#             elif s_a[k] < s_b[k]:
+#                 return [b, a]
+#             else:
+#                 k += 1
+#             if k == l:
+#                 if s_b[k] >= s_a[k-1]:
+#                     return [b, a]
+#                 else:
+#                     return [a, b]
+#     else:
+#         while True:
+#             if s_a[k] > s_b[k]:
+#                 return [a, b]
+#             elif s_a[k] < s_b[k]:
+#                 return [b, a]
+#             else:
+#                 k += 1
+#             if k==l:
+#                 if s_a[k] >= s_b[k-1]:
+#                     return [a, b]
+#                 else:
+#                     return [b, a]
+
+print(rel_nums(25,254))
+print('30'>'254')
---
+++ numpy/mysql.md
@@ -0,0 +1,17 @@
+![image-20200909143434887](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909143434887.png)
+
+![image-20200909151811875](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909151811875.png)
+
+![image-20200909151937646](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909151937646.png)
+
+![image-20200909152159713](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909152159713.png)
+
+![image-20200909152456866](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909152456866.png)
+
+![image-20200909152618992](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909152618992.png)
+
+![image-20200909152750378](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909152750378.png)
+
+![image-20200909153233670](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909153233670.png)
+
+![image-20200909154001648](C:\Users\User\AppData\Roaming\Typora\typora-user-images\image-20200909154001648.png)
